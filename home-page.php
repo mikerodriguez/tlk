cf09f70e1bf605da592c61a2896b336fc2ef7217
Mike Rodriguez <m@mikerodriguez.me>
2018-06-24 09:35:42 -0400

    working on home CTA

--- home-page.php
+++ home-page.php
@@ -40,7 +40,28 @@
 <section class="home-feature-products"></section>
 
 
-<section class="home-cta-5050"></section>
+<section class="home-cta-5050">
+	<div class="wrapper">
+		<div class="home-cta-container">
+
+			<div class="cta-item">
+				<div class="inner-cta">
+					Flexible Plans <br>
+					Skip weeks, swap recipes, and cancel anytime - only order what and when you want. No commitments.
+					<br>
+					<a href="#">View our Services</a>
+				</div>
+			</div>
+
+			<div class="cta-item">
+				<div class="inner-cta">
+					newsletter
+				</div>
+			</div>
+
+		</div>
+	</div>
+</section>
 
 
 <section class="home-cta"></section>
